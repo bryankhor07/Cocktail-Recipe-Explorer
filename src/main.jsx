a13c9346ba3c565ef388dc6827df41001d25762e
Bryan Khor <bryankhor07@gmail.com>
2025-09-30 17:41:32 -0700

    Wrapped App with FavoritesProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { FavoritesProvider } from './context/FavoritesContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+      <FavoritesProvider>
+        <App />
+      </FavoritesProvider>
     </BrowserRouter>
   </StrictMode>,
 )
